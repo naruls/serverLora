6cca1dc714158fed35363794ea79d08ca75982da
naruls <76217299+naruls@users.noreply.github.com>
2022-03-04 12:10:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import momentZone from 'moment-timezone';
 const app = express();
 
 const options = {
-  origin: '*',,
+  origin: '*',
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 200,
